7bd4e1cc1c16cf2368679a7debed55919ea997f8
israa <israahindi.123@gmail.com>
2023-05-18 23:42:01 +0300

    is

--- 1_python/lab/tk.py
+++ 1_python/lab/tk.py
@@ -4,8 +4,19 @@ import tkinter as tk
 win = tk.Tk()
 
 # Create a label widget and add it to the window
-label = tk.Label(win, text="Hello, Tkinter!")
+win.geometry('100x200')
+label = tk.Label(win, text="  isro  ")
 label.pack()
 
+B  =tk.Button(win , text = "LOL",command=win.destroy)
+B.pack(side = tk.TOP)
+
+BB  =tk.Button(win , text = "LOL",command=win.destroy)
+BB.pack(side = tk.LEFT)
+
+BBB  =tk.Button(win , text = "LOL",command=win.destroy)
+BBB.pack(side = tk.RIGHT)
+BBB  =tk.Button(win , text = "LOL",command=win.destroy)
+BBB.pack(side = tk.BOTTOM)
 # Run the Tkinter event loop
 win.mainloop()
